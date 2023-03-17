5ee1235bed4fa84e2331c7ac352f90291ccc9365
Thaweewat <thaweewat.data@gmail.com>
2023-03-17 12:29:32 +0700

    rer

--- app.py
+++ app.py
@@ -1,70 +1,56 @@
 import streamlit as st
 import pandas as pd
-import random
 
-# Use the full page instead of a narrow central column
-st.set_page_config(layout="wide")
+# Define a function to load the CSV file
+def load_csv(file):
+    df = pd.read_csv(file)
+    return df
 
+# Define a function to display the current row
+def display_row(df, row_idx):
+    row = df.iloc[row_idx]
+    st.write(row)
 
-@st.cache
-def convert_df(dataframe: pd.DataFrame):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return dataframe.to_csv().encode('utf-8')
+# Define the Streamlit app
+def main():
+    st.title("CSV Viewer")
 
+    # Upload the CSV file
+    file = st.file_uploader("Upload CSV file", type=["csv"])
+    if file:
+        # Load the CSV file
+        df = load_csv(file)
 
-def display_row():
-    unfilled_rows = df[df["User Decision"] == ""].index.tolist()
+        # Initialize the row index
+        row_idx = 0
 
-    if not unfilled_rows:
-        st.write("All done!")
-        return None
+        # Display the current row
+        display_row(df, row_idx)
 
-    row_num = random.choice(unfilled_rows)
-    row_to_display = df.loc[row_num].to_frame().T
-    edited_row = st.experimental_data_editor(row_to_display)
+        # Define the buttons
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            if st.button("Match"):
+                df.loc[row_idx, "user decision"] = "match"
+                row_idx += 1
+                display_row(df, row_idx)
+        with col2:
+            if st.button("Non Match"):
+                df.loc[row_idx, "user decision"] = "non match"
+                row_idx += 1
+                display_row(df, row_idx)
+        with col3:
+            if st.button("Back"):
+                if row_idx > 0:
+                    row_idx -= 1
+                    display_row(df, row_idx)
+                else:
+                    st.warning("You are at the beginning of the file.")
 
-    return row_num, edited_row
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
 
-
-uploaded_file = st.file_uploader("Choose your CSV file")
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-else:
-    df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-
-row_num, edited_row = display_row()
-
-next_match = st.button("Next (Matched)")
-next_not_match = st.button("Next (Not Matched)")
-back = st.button("Back")
-
-if next_match:
-    df.loc[row_num, "User Decision"] = "Match"
-    row_num, edited_row = display_row()
-
-elif next_not_match:
-    df.loc[row_num, "User Decision"] = "Unmatch"
-    row_num, edited_row = display_row()
-
-elif back:
-    row_num, edited_row = display_row()
-
-final_df = convert_df(df)
-if final_df is not None:
-    st.markdown("You can download edited file from download button below (CSV)")
-    st.download_button(
-        label="Download edited data as CSV",
-        data=final_df,
-        file_name='edited_data.csv',
-        mime='text/csv',
-    )
-
-filled_rows = df[df["User Decision"] != ""].count()["User Decision"]
-unfilled_rows = df[df["User Decision"] == ""].count()["User Decision"]
-
-st.write(f"Filled rows: {filled_rows}")
-st.write(f"Unfilled rows: {unfilled_rows}")
 
 
 
