ad564869316d742f073313c96daf398aef1d7e82
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-24 04:49:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import base64
 
-st. set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 # Load CSV file
 def load_csv(uploaded_file):
@@ -27,6 +27,8 @@ def get_next_row(df, current_index):
 def main():
     if 'history' not in st.session_state:
         st.session_state.history = []
+    if 'hide_buttons' not in st.session_state:
+        st.session_state.hide_buttons = False
     st.title("CSV Annotator V1.0")
 
     # Upload CSV
@@ -54,100 +56,103 @@ def main():
 
             # Display row placeholder
             row_placeholder = st.empty()
+
             # Button logic
-            if st.session_state.current_index is not None:
+            if not st.session_state.hide_buttons:
                 col1, col2, col3, col4, col5 = st.columns(5)
                 with col1:
-                    if st.button("Unknown"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "unknown"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                            row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
-                            row_placeholder.dataframe(row, width=1600)
-                        else:
-                            row_placeholder.warning("No more rows available.")
-                with col3:
-                    if st.button("Next Match"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "match"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                            row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
-                            row_placeholder.dataframe(row, width=1600)
-                        else:
-                            row_placeholder.warning("No more rows available.")
-                with col4:
-                    if st.button("Next Non-Match"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "non match"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                            row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
-                            row_placeholder.dataframe(row, width=1600)
-                        else:
-                            row_placeholder.warning("No more rows available.")
-                with col5:
-                    if st.button("Back"):
-                        if st.session_state.history:
-                            st.session_state.current_index = st.session_state.history.pop()
-                            df.at[st.session_state.current_index, 'user decision'] = ""
-                            row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
-                            row_placeholder.dataframe(row, width=1600)
-                        elif st.session_state.current_index != df.index[0]:
-                            st.session_state.current_index -= 1
-                            df.at[st.session_state.current_index, 'user decision'] = ""
-                            row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
-                            row_placeholder.dataframe(row, width=1600)
-                        else:
-                            row_placeholder.warning("No more rows available.")
+                    unknown_button = st.button("Unknown")
                 with col2:
-                    if st.button("Not Address"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "non address"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                            row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
-                            row_placeholder.dataframe(row, width=1600)
-                        else:
-                            row_placeholder.warning("No more rows available.")
+                    not_address_button = st.button("Not Address")
+                with col3:
+                    match_button = st.button("Next Match")
+                with col4:
+                    non_match_button = st.button("Next Non-Match")
+                with col5:
+                    back_button = st.button("Back")
 
-                if st.session_state.current_index is not None and (st.session_state.current_index - 1) not in st.session_state.history:
-                    st.session_state.history.append(st.session_state.current_index - 1)
-
-                # Display row initially
-                if st.session_state.current_index is None and not df[df['user decision'] == ""].empty:
-                    st.session_state.current_index = df[df['user decision'] == ""].index[0]
-
+            if unknown_button:
                 if st.session_state.current_index is not None:
-                    try:
+                    st.session_state.history.append(st.session_state.current_index)
+                    df.at[st.session_state.current_index, 'user decision'] = "unknown"
+                    st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+                    if st.session_state.current_index is not None:
                         row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
                         row_placeholder.dataframe(row, width=1600)
+                else:
+                    st.session_state.hide_buttons = True
 
-                        user_decision = st.selectbox("Choose the correct option", ("", "HOUSE_FULL_1", "HOUSE_FULL_2"))
+            elif match_button:
+                if st.session_state.current_index is not None:
+                    st.session_state.history.append(st.session_state.current_index)
+                    df.at[st.session_state.current_index, 'user decision'] = "match"
+                    st.session_state.current_index = get_next_row(df, st.session_state.current_index is not None:
+                        row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
+                        row_placeholder.dataframe(row, width=1600)
+                else:
+                    st.session_state.hide_buttons = True
+            elif non_match_button:
+                if st.session_state.current_index is not None:
+                    st.session_state.history.append(st.session_state.current_index)
+                    df.at[st.session_state.current_index, 'user decision'] = "non match"
+                    st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+                if st.session_state.current_index is not None:
+                    row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
+                    row_placeholder.dataframe(row, width=1600)
+                else:
+                    st.session_state.hide_buttons = True
+            elif not_address_button:
+                if st.session_state.current_index is not None:
+                    st.session_state.history.append(st.session_state.current_index)
+                    df.at[st.session_state.current_index, 'user decision'] = "non address"
+                    st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+                if st.session_state.current_index is not None:
+                    row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
+                    row_placeholder.dataframe(row, width=1600)
+                else:
+                    st.session_state.hide_buttons = True
+            elif back_button:
+                if st.session_state.history:
+                    st.session_state.current_index = st.session_state.history.pop()
+                    df.at[st.session_state.current_index, 'user decision'] = ""
+                    row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
+                    row_placeholder.dataframe(row, width=1600)
+                elif st.session_state.current_index != df.index[0]:
+                    st.session_state.current_index -= 1
+                    df.at[st.session_state.current_index, 'user decision'] = ""
+                    row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
+                    row_placeholder.dataframe(row, width=1600)
+                else:
+                    row_placeholder.warning("No more rows available.")
+                    
+                        if st.session_state.current_index is not None and (st.session_state.current_index - 1) not in st.session_state.history:
+                st.session_state.history.append(st.session_state.current_index - 1)
 
-                        if user_decision:
-                            df.loc[st.session_state.current_index, 'user decision'] = user_decision
-                            st.session_state.current_index = None
-                            st.success("Decision saved. Loading next row...")
-                    except KeyError:
-                        st.session_state.current_index = None
-                        row_placeholder.empty()
-                        st.info("Thanks, all rows are filled.")
-            else:
-                row_placeholder.empty()
-                st.info("Thanks, all rows are filled.")
+            # Display row initially
+            if st.session_state.current_index is None and not df[df['user decision'] == ""].empty:
+                st.session_state.current_index = df[df['user decision'] == ""].index[0]
 
+            if st.session_state.current_index is not None:
+                row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
+                row_placeholder.dataframe(row, width=1600)
 
+                user_decision = st.selectbox("Choose the correct option", ("", "HOUSE_FULL_1", "HOUSE_FULL_2"))
 
+                if user_decision:
+                    df.loc[st.session_state.current_index, 'user decision'] = user_decision
+                    st.session_state.current_index = None
 
+                    st.success("Decision saved. Loading next row...")
 
+    if st.session_state.hide_buttons:
+        st.warning("Thanks, all rows are filled.")
+    else:
+        annotated_rows = df[df['user decision'] != ""].shape[0]
+        left_rows = df[df['user decision'] == ""].shape[0]
+        st.write(f"Annotated rows: {annotated_rows}")
+        st.write(f"Left rows: {left_rows}")
 
-            annotated_rowss = df[df['user decision'] != ""].shape[0]
-            left_rowss = df[df['user decision'] == ""].shape[0]
-            st.write(f"Annotated rows: {annotated_rowss}")
-            st.write(f"Left rows: {left_rowss}")
-            # Download updated CSV
+    # Download updated CSV
             csv = df.to_csv(index=False)
             b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
             href = f'<a href="data:file/csv;base64,{b64}" download="updated.csv">Download Updated CSV</a>'
@@ -159,5 +164,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
     
