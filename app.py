1276abf56c36ad9674e6a2c33c792dbf590f65a1
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-24 02:41:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,51 +56,52 @@ def main():
                 left_rows = df[df['user decision'] == ""].shape[0]
                 st.write(f"Annotated rows: {annotated_rows}")
                 st.write(f"Left rows: {left_rows}")
-                
-                # Button logic
-                col1, col2, col3, col4, col5 = st.columns(5)
-                with col1:
-                    if st.button("Unknown"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "unknown"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                with col3:
-                    if st.button("Next Match"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "match"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                with col4:
-                    if st.button("Next Non-Match"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "non match"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                with col5:
-                    if st.button("Back"):
-                        if st.session_state.history:
-                            st.session_state.current_index = st.session_state.history.pop()
-                            df.at[st.session_state.current_index, 'user decision'] = ""
-                        elif st.session_state.current_index != df.index[0]:
-                            st.session_state.current_index -= 1
-                            df.at[st.session_state.current_index, 'user decision'] = ""
-                with col2:
-                    if st.button("Not Address"):
-                        if st.session_state.current_index is not None:
-                            st.session_state.history.append(st.session_state.current_index)
-                            df.at[st.session_state.current_index, 'user decision'] = "non address"
-                            st.session_state.current_index = get_next_row(df, st.session_state.current_index)
-                            
-                if st.session_state.current_index is not None and (st.session_state.current_index - 1) not in st.session_state.history:
-                    st.session_state.history.append(st.session_state.current_index - 1)
-                
+                if left_rows > 0:
+                    # Button logic
+                    col1, col2, col3, col4, col5 = st.columns(5)
+                    with col1:
+                        if st.button("Unknown"):
+                            if st.session_state.current_index is not None:
+                                st.session_state.history.append(st.session_state.current_index)
+                                df.at[st.session_state.current_index, 'user decision'] = "unknown"
+                                st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+                    with col3:
+                        if st.button("Next Match"):
+                            if st.session_state.current_index is not None:
+                                st.session_state.history.append(st.session_state.current_index)
+                                df.at[st.session_state.current_index, 'user decision'] = "match"
+                                st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+                    with col4:
+                        if st.button("Next Non-Match"):
+                            if st.session_state.current_index is not None:
+                                st.session_state.history.append(st.session_state.current_index)
+                                df.at[st.session_state.current_index, 'user decision'] = "non match"
+                                st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+                    with col5:
+                        if st.button("Back"):
+                            if st.session_state.history:
+                                st.session_state.current_index = st.session_state.history.pop()
+                                df.at[st.session_state.current_index, 'user decision'] = ""
+                            elif st.session_state.current_index != df.index[0]:
+                                st.session_state.current_index -= 1
+                                df.at[st.session_state.current_index, 'user decision'] = ""
+                    with col2:
+                        if st.button("Not Address"):
+                            if st.session_state.current_index is not None:
+                                st.session_state.history.append(st.session_state.current_index)
+                                df.at[st.session_state.current_index, 'user decision'] = "non address"
+                                st.session_state.current_index = get_next_row(df, st.session_state.current_index)
+
+                    if st.session_state.current_index is not None and (st.session_state.current_index - 1) not in st.session_state.history:
+                        st.session_state.history.append(st.session_state.current_index - 1)
+                else:
+                    st.success("Thanks for annotating the data! All the data is ready to download.")
+
                 # Download updated CSV
                 csv = df.to_csv(index=False)
                 b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
                 href = f'<a href="data:file/csv;base64,{b64}" download="updated.csv">Download Updated CSV</a>'
                 st.markdown(href, unsafe_allow_html=True)
-       
                 st.dataframe(df, width=1600)
         
         # Check if all rows are annotated
