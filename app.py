d0dcf976d6f6d9ed22c31137cd8bbddf9f718974
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-24 03:09:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def main():
                 left_rows = df[df['user decision'] == ""].shape[0]
                 st.write(f"Annotated rows: {annotated_rows}")
                 st.write(f"Left rows: {left_rows}")
-                if left_rows != 0:
+                if left_rows != 1:
                     # Button logic
                     col1, col2, col3, col4, col5 = st.columns(5)
                     with col1:
