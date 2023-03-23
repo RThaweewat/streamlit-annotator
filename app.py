6f2c1b69390022097d4bb045498fecaab9463b1a
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-24 03:18:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,9 @@ def main():
                 row = df.loc[st.session_state.current_index, ['HOUSE_FULL_1', 'HOUSE_FULL_2']]
                 st.dataframe(row, width=1600)
                 # Display counters
-                left_rows = df[df['user decision'] == ""].shape[0]
-
+                left_rows = df[df['user decision'] == ""].shape[0] + 1
+                st.write(f"Left rows: {left_rows}")
+                
                 if left_rows == 1:
                     # Button logic
                     col1, col2, col3, col4, col5 = st.columns(5)
