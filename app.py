0f0b3ab8817e24d637e37ce94b9c53e7fdb38589
Thaweewat <104571571+RThaweewat@users.noreply.github.com>
2023-03-24 10:34:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -143,15 +143,17 @@ def main():
         st.warning("Thanks, all rows are filled.")
     
     
-    # Download updated CSV
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}" download="updated.csv">Download Updated CSV</a>'
-    st.markdown(href, unsafe_allow_html=True)
-
-    st.dataframe(df[['HOUSE_FULL_1', 'HOUSE_FULL_2', 'user decision']], width=1600)
-
-
+            st.dataframe(df[['HOUSE_FULL_1', 'HOUSE_FULL_2', 'user decision']], width=1600)
+            
+            # Download updated CSV
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a href="data:file/csv;base64,{b64}" download="updated.csv">Download Updated CSV</a>'
+            st.markdown(href, unsafe_allow_html=True)
+        else:
+            st.warning("Unable to load the CSV file. Please make sure it is in the correct format.")
+    else:
+        st.warning("Please upload a CSV file to get started.")
 
 if __name__ == "__main__":
     main()
